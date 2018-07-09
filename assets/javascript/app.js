b9375558b9a3dcd6ab7af2c2026d7ac80cbf2b76
sandynism <31261960+Sandynism@users.noreply.github.com>
2018-07-09 14:28:46 -0400

    timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,75 @@
-$(document).ready(function () {
-    let correctAnswers = 0
-    let incorrectAnswers = 0
-    let unresponsive = 0
-    let questionTime = 20
-    let gameTime //overall game play time = 2 mins??
-    
+//SETTINGS
+let correctAnswers = 0
+let incorrectAnswers = 0
+let unresponsive = 0
+let countdown = 20
+let counter = 0
+let userSelection
+let questionsObject = [{
+    question: "What does Sandy have every single morning?",
+    answers: ["Bad Breath", "Green Juice", "Chicken & Waffles", "A Stretch Session"],
+    correct: "Green Juice"
+}, {
+    question: "What is Sandy's favorite visited country?",
+    answers: ["Greece", "Japan", "Thailand", "Australia"],
+    correct: "Greece"
+}, {
+    question: "What is the oldest condiment in Sandy's fridge right now?",
+    answers: ["Dijon Mustard", "Sriracha", "Peter Lugers Steak Sauce", "Ranch Dressing"],
+    correct: "Ranch Dressing"
+}, {
+    question: "What does Sandy never leave the house without?",
+    answers: ["SPF", "A Scrunchie", "Lip Balm", "Cash"],
+    correct: "Lip Balm"
+}, {
+    question: "What was Sandy's favorite subject in school?",
+    answers: ["Physics", "Biology", "Dance", "Asian Studies"],
+    correct: "Biology"
+}, {
+    question: "What is Sandy's favorite color?",
+    answers: ["White", "Turquoise", "Pink", "Green"],
+    correct: "Turquoise"
+}, {
+    question: "How many concerts has Sandy been to?",
+    answers: ["", "", "", ""],
+    correct: ""
+}, {
+    question: "Who is Sandy's favorite musical act?",
+    answers: ["Illenium", "The Weeknd", "Ed Sheeran", "Above & Beyond"],
+    correct: "Above & Beyond"
+}]
+//rock band, game, names of sandys favorite dogs
 
+
+$(document).ready(function () {
+
+    $("#startGame").click(function () {
+        $(".btn-secondary").addClass("disappear")
+        $(".btn-outline-secondary").removeClass("disappear")
+        $(".questions").removeClass("disappear")
+        $("#timer").removeClass("disappear")
+    })
+
+    $(".question").text(questionsObject[0].question)
+
+    let options = questionsObject[0].answers
+    for (let i = 0; i < options.length; i++) {
+        $('.choices').append("<div>" + options[i] + "</div>")
+    }
+    
+    //shows the remaining seconds left
+    function timer() {
+		counter = setInterval(twentySeconds, 1000);
+		function twentySeconds() {
+			if (countdown === 0) {
+                clearInterval(counter)
+                //next question
+			} else if (countdown > 0) {
+				countdown--
+			}
+			$("#timer").html(countdown + " Seconds Remaining")
+		}
+	}
+    timer()
 
 })
